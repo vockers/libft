633e173785db9a5ea6f3289c328c178ec8905671
Vincent Ockers <vockers@f0r2s3.codam.nl>
2023-10-03 14:56:06 +0200

    ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,11 @@
 /*   By: vockers <vockers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/02 14:42:15 by vockers       #+#    #+#                 */
-/*   Updated: 2023/10/02 15:06:48 by vockers       ########   odam.nl         */
+/*   Updated: 2023/10/03 14:55:39 by vockers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -31,3 +29,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub[len] = '\0';
 	return (sub);
 }
+
+// int main()
+// {
+// 	char *str = "hello, world!";
+// 	printf("%s\n", ft_substr(str, 5, 5));
+// 	return 0;
+// }
