d7ccdbb0594c102593f3ba866469927b521123c1
makervin <makervin@proton.me>
2023-10-05 17:32:42 +0200

    fixed header file

--- libft.h
+++ libft.h
@@ -38,6 +38,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
 
