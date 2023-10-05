4b3b566a42086a66717d91cc9aca59d481eb2a1c
makervin <makervin@proton.me>
2023-10-05 22:26:58 +0200

    fixed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -19,7 +19,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (n && (*str1 || *str2))
+	while (n)
 	{
 		if (*str1 != *str2)
 			return (*str1 - *str2);
