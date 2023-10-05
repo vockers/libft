6c446e5fb174dfd3bc3bd1bea693330d102d9805
makervin <makervin@proton.me>
2023-10-05 22:33:52 +0200

    fixed memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -18,6 +18,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*s;
 	size_t			i;
 
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	i = 0;
