cbdc012c988d6d7ce4469170f3fee8797eab2097
makervin <makervin@proton.me>
2023-10-08 00:44:41 +0200

    fixed strlcat

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -14,15 +14,17 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int	i;
+	size_t	i;
 
-	i = 0;
-	while (i < (int)size - 1 && src[i])
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
-	}
-	if (i < (int)size)
+		i = 0;
+		while (i < size - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
+	}
 	return (ft_strlen(src));
 }
