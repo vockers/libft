d93723c6ce681c1e8a27b0b4ebc50237105237d4
makervin <makervin@proton.me>
2023-10-08 00:17:39 +0200

    fixed lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -16,14 +16,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*new_content;
 
 	new_list = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
 			ft_lstclear(&new_list, del);
+			del(new_content);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_node);
