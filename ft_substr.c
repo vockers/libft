2f6b77cb0ddba05a451c897d5d2170a203ad9f27
makervin <makervin@proton.me>
2023-10-07 18:26:14 +0200

    ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -11,28 +11,24 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	s_len;
-	size_t	i;
 
+	if (!s)
+		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		len = 0;
-	if (s_len < len)
-		len = s_len;
+		return (ft_strdup(""));
+	if (s_len - start < len)
+		len = s_len - start;
 	sub = (char *)malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (NULL);
-	i = 0;
-	while (i < len && s[start + i])
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
+	strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
 
