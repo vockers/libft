8664cfe49a85d932e0b74a5d19d5905806a61a71
makervin <makervin@proton.me>
2023-10-11 20:24:55 +0200

    ft_atoi_test.c

--- tests/ft_atoi_test.c
+++ tests/ft_atoi_test.c
@@ -10,7 +10,7 @@
 #define ANSI_COLOR_BLUE		"\x1b[34m"
 #define ANSI_COLOR_MAGENTA	"\x1b[35m"
 #define ANSI_COLOR_CYAN		"\x1b[36m"
-#define ANSI_COLOR_WHITE	"\x1b[0m"
+#define ANSI_COLOR_RESET	"\x1b[0m"
 
 void	test(const char *arg)
 {
@@ -18,27 +18,29 @@ void	test(const char *arg)
 	int		std_res	= atoi(arg);
 	
 	if (ft_res != std_res)
-		printf(ANSI_COLOR_RED "KO" ANSI_COLOR_WHITE ":\t%d\n\t%d", ft_res, std_res);	
+		printf(ANSI_COLOR_RED "KO" ANSI_COLOR_RESET ":\t%d\n\t%d", ft_res, std_res);	
 	else
-		printf(ANSI_COLOR_GREEN "OK" ANSI_COLOR_WHITE "\n");
+		printf(ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET " ");
 }
 
 int	main()
 {
-	/*  1 */ test("1");
-	/*  2 */ test("a1");
-	/*  3 */ test("--1");
-	/*  4 */ test("++1");
-	/*  5 */ test("+1");
-	/*  6 */ test("-1");
-	/*  7 */ test("0");
-	/*  8 */ test("+42test");
-	/*  9 */ test("+101");
-	/* 10 */ test("2147483647");
-	/* 11 */ test("-2147483648");
-	/* 12 */ test("-+42");
-	/* 13 */ test("+-42");
-	/* 14 */ test("+");
-	/* 15 */ test("-");
+	printf("ft_atoi:\t");
+	test("1");
+	test("a1");
+	test("--1");
+	test("++1");
+	test("+1");
+	test("-1");
+	test("0");
+	test("+42test");
+	test("+101");
+	test("2147483647");
+	test("-2147483648");
+	test("-+42");
+	test("+-42");
+	test("+");
+	test("-");
+	printf("\n");
 	return 0;
 }
