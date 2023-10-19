71e8ff3a346014a9175cd87fe2adbfbea03fd479
makervin <makervin@proton.me>
2023-10-19 22:21:39 +0200

    cleaned strjoin

--- string/ft_strjoin.c
+++ string/ft_strjoin.c
@@ -18,21 +18,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 
 	new = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 	i = 0;
 	while (*s1)
-	{
-		new[i] = *s1;
-		i++;
-		s1++;
-	}
+		new[i++] = *s1++;
 	while (*s2)
-	{
-		new[i] = *s2;
-		i++;
-		s2++;
-	}
+		new[i++] = *s2++;
 	new[i] = '\0';
 	return (new);
 }
