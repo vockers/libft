c9b9c7bf7d1dd1e538520e362f605e1c3c87c342
makervin <makervin@proton.me>
2023-10-19 23:24:13 +0200

    removed strlcpy from substr

--- string/ft_substr.c
+++ string/ft_substr.c
@@ -17,6 +17,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	s_len;
+	size_t	i;
 
 	if (!s)
 		return (NULL);
@@ -28,7 +29,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub = (char *)malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (NULL);
-	ft_strlcpy(sub, s + start, len + 1);
+	i = 0;
+	while (s[start + i] && i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
 	return (sub);
 }
 
